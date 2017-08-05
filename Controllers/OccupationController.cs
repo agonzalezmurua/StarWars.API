4bd0883006813feffcf8ccd49eb829ab584a38ae
AgustinGonzalez <xenrak@gmail.com>
2017-08-04 23:54:44 -0400

    Agregado controlador de ocupación

--- Controllers/OccupationController.cs
+++ Controllers/OccupationController.cs
@@ -3,24 +3,27 @@ using Microsoft.AspNetCore.Mvc;
 using System.Linq;
 using StarWars.Models;
 using StarWars.Stores;
+using System;
 
 namespace StarWars.Controllers
 {
     [Route("api/[controller]")]
     [Produces("application/json")]
-    public class PeopleController : Controller
+    public class OccupationController : Controller
     {
         [HttpGet, Route("")]
         public JsonResult GetAll()
         {
-            return Json(PeopleStore.Store);
+            return Json(
+                Enum.GetNames(typeof(Occupations))
+            );
         }
 
-        [HttpGet, Route("{id:int}")]
-        public JsonResult Get(int id)
+        [HttpGet, Route("{occupation}/people")]
+        public JsonResult GetPeopleFromAffiliation(Occupations occupation)
         {
             return Json(
-                PeopleStore.Store.FirstOrDefault( people => people.id == id )
+                PeopleStore.Store.Where(people => people.occupation.ToString() == occupation.ToString())
             );
         }
     }
